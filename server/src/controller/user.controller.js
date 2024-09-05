ee7f2ded7ff22ca875f393941ad7bd2a58db38f8
Deepak Bhatt <bhattdeepak1402@gmail.com>
2024-09-05 07:55:23 +0530

    User registration

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -1,9 +1,27 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/apiError.js";
+import {ApiResponse} from "../utils/apiResponse.js";
+import {User} from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res)=>{
-    res.status(200).json({
-        message: "OK"
+    const {name, email, password} = req.body;
+    //console.log(email);
+
+    //Checking if the fields are empty
+    if([name,email,password].some((field) => field?.trim() === "")){
+        throw new ApiError(400, "Name is Required");
+    }
+
+    //Checking If the Email is already registered with us or not
+    const existedUser = User.findOne({
+        $or: [{name}]
     })
+    if(existedUser){
+        throw new ApiError(409, "User with this email already exist");
+    }
+    
+
+    
 })
 
 export {registerUser};
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -3,7 +3,16 @@ import { registerUser } from "../controller/user.controller.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
-router.route("/login").post(registerUser);
+router.route("/register").post(
+    upload.fields([
+        {
+
+        }
+    ]),
+    registerUser
+);
+
+
+// router.route("/login").post(registerUser);
 
 export default router;
